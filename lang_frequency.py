a0f4de13bd05b2e7896c5a52b590c34fcaf4d006
MichailPerm <ApxaHreJl@gmail.com>
2018-04-30 20:11:32 +0500

    Adding constant to understand code

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,6 +3,7 @@ import re
 from collections import Counter
 
 PUNCTUATION_MARK_PATTERN = re.compile(r"\W+", re.UNICODE)
+WORDS_TO_RETURN = 10
 
 
 def load_data(filepath):
@@ -18,7 +19,7 @@ def get_most_frequent_words(text_data):
     for i, text_data in enumerate(text_data_list):
         if text_data == "":
             text_data_list.pop(i)
-    return Counter(text_data_list).most_common(10)
+    return Counter(text_data_list).most_common(WORDS_TO_RETURN)
 
 
 def create_args_parser(argument):
