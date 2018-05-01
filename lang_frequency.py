9ade34e1b0db08ba729f99837e5e74529976e133
Michail_Z <apxahrejl@gmail.com>
2018-05-01 14:50:53 +0500

    Refactoring

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,38 +1,35 @@
+# -*- coding: utf-8 -*-
 import argparse
 import re
+import sys
 from collections import Counter
-
-PUNCTUATION_MARK_PATTERN = re.compile(r"\W+", re.UNICODE)
 
 
 def load_data(filepath):
-    with open(filepath, 'r') as text_file:
+    with open(filepath, "r") as text_file:
         text_data = text_file.read()
         return text_data
 
 
 def get_most_frequent_words(text_data):
-    text_data_without_marks = PUNCTUATION_MARK_PATTERN.sub(text_data, " ")
-    text_data_without_marks = re.sub("–", "", str(text_data_without_marks))
-    text_data_list = text_data_without_marks.lower().strip().split(" ")
-    for i, text_data in enumerate(text_data_list):
-        if text_data == "":
-            text_data_list.pop(i)
-    return Counter(text_data_list).most_common(10)
+    the_most_common_words_quantity = 10
+    punctuation_mark_pattern = re.compile(r"\w{1,}")
+    words = punctuation_mark_pattern.findall(text_data.lower())
+    return Counter(words).most_common(the_most_common_words_quantity)
 
 
-def create_args_parser(argument):
+def create_args_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument(argument)
+    parser.add_argument("filepath")
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-    args = create_args_parser('filepath')
+if __name__ == "__main__":
+    args = create_args_parser()
     try:
         text_data = load_data(args.filepath)
     except IOError:
-        print("Unable to read file {}.".format(args.filepath))
+        sys.exit("Unable to read file {}.".format(args.filepath))
     most_frequent_words = get_most_frequent_words(text_data)
-    for word in most_frequent_words:
-        print(word[0])
+    for word, count in most_frequent_words:
+        print(word)
